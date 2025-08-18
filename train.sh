a6b0d8fafd4f834cc1eb661124f3e5b96e9c25cf
Alvis Duong <alvisduong@gmail.com>
2025-08-18 20:35:10 +0100

    Get weights

--- train.sh
+++ train.sh
@@ -2,6 +2,8 @@
 
 pip install -r requirements.txt -q
 
+wget -P /content/yolov9/weights -q https://github.com/WongKinYiu/yolov9/releases/download/v0.1/yolov9-c.pt
+
 python train_dual.py \
 --workers 32 \
 --batch-size 16 \
